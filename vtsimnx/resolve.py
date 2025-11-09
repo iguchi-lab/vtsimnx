2f1968ae85fe078f960ff8d17104f24fca4b15d0
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2025-11-09 15:05:20 +0900

    Add type hints to resolve function in resolve.py

--- vtsimnx/resolve.py
+++ vtsimnx/resolve.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict, List, Tuple, Optional
+
 from .parse import parse
 
 def resolve(raw_config: Dict[str, Any], output_path: Optional[str] = "parsed_input_data.json") -> Dict[str, Any]:
