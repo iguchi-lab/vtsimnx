15c76442de3b64d1d4fc051d4c3b491605d0a78a
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2025-11-09 11:24:15 +0900

    Update parse.py

--- vtsimnx/parse.py
+++ vtsimnx/parse.py
@@ -3,6 +3,6 @@ from .logger import get_logger
 logger = get_logger(__name__)
 
 def parse(input_data):
-  logger.info("インプットデータの読み込み開始")
-  print(input_data)
-  return "get input_data"
+  logger.info("設定データの読み込み開始")
+  logger.info("設定データの処理が完了しました")
+  return 0
