3f93cccf87c934aa7b94d6fd08c23fe3b06598c7
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2025-12-17 06:28:34 +0900

    arrange

--- solver/core/network_structures.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../vtsim_solver.h"
-
-// network_structures.hはvtsim_solver.hの型定義を再エクスポートする
-// Graph, Vertex, Edge, VertexProperties, EdgeProperties などは
-// vtsim_solver.hで定義されている
-
